87ef777c09b443123e89762fdc979ee08dd4db3a
jokerwyt <914554688@qq.com>
2024-08-22 23:28:57 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,30 @@ const nextConfig = {
 
     return config;
   },
+
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/jokerwyt.github.io",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = createMDX({
@@ -21,3 +45,4 @@ const withMDX = createMDX({
 
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig);
+
