fb127c0268f399d8211a8588e7379c7f87b0b592
jokerwyt <914554688@qq.com>
2024-09-10 19:55:31 +0800

    Zip graph and remove email link

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -49,35 +49,36 @@ export default function AboutSection() {
             {personalInfo.socialMedia.map((social) => {
               return (
                 <Button asChild variant="outline" key={social.name} size={'sm'}>
-                  <Link href={social.url} target="_blank">
-                    {social.name === 'Email' ? (
-                      // <RiMailLine size={16} />
-                      // remove mailto: prefix
-                      social.url.replace('mailto:', '')
-                    ) : social.name === 'GitHub' ? (
-                      <RiGithubFill size={16} />
-                    ) : social.name === 'Twitter' ? (
-                      <RiTwitterXLine size={16} />
-                    ) : social.name === 'Google Scholar' ? (
-                      <RiGoogleFill size={16} />
-                    ) : social.name === 'ORCID' ? (
-                      <svg
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        width="16"
-                        height="16"
-                      >
-                        <path d="m12 0c-6.628 0-12 5.372-12 12s5.372 12 12 12 12-5.372 12-12-5.372-12-12-12zm-4.631 4.378c.525 0 .947.431.947.947s-.422.947-.947.947a.95.95 0 0 1 -.947-.947c0-.525.422-.947.947-.947zm-.722 3.038h1.444v10.041h-1.444zm3.562 0h3.9c3.712 0 5.344 2.653 5.344 5.025 0 2.578-2.016 5.025-5.325 5.025h-3.919zm1.444 1.303v7.444h2.297c3.272 0 4.022-2.484 4.022-3.722 0-2.016-1.284-3.722-4.097-3.722z" />
-                      </svg>
-                    ) : (
-                      <RiLinkedinBoxFill size={16} />
-                    )}
-                  </Link>
+                  {social.name === 'Email' ? (
+                    <span>{social.url.replace('mailto:', '')}</span>
+                  ) : (
+                    <Link href={social.url} target="_blank">
+                      {social.name === 'GitHub' ? (
+                        <RiGithubFill size={16} />
+                      ) : social.name === 'Twitter' ? (
+                        <RiTwitterXLine size={16} />
+                      ) : social.name === 'Google Scholar' ? (
+                        <RiGoogleFill size={16} />
+                      ) : social.name === 'ORCID' ? (
+                        <svg
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          width="16"
+                          height="16"
+                        >
+                          <path d="m12 0c-6.628 0-12 5.372-12 12s5.372 12 12 12 12-5.372 12-12-5.372-12-12-12zm-4.631 4.378c.525 0 .947.431.947.947s-.422.947-.947.947a.95.95 0 0 1 -.947-.947c0-.525.422-.947.947-.947zm-.722 3.038h1.444v10.041h-1.444zm3.562 0h3.9c3.712 0 5.344 2.653 5.344 5.025 0 2.578-2.016 5.025-5.325 5.025h-3.919zm1.444 1.303v7.444h2.297c3.272 0 4.022-2.484 4.022-3.722 0-2.016-1.284-3.722-4.097-3.722z" />
+                        </svg>
+                      ) : (
+                        <RiLinkedinBoxFill size={16} />
+                      )}
+                    </Link>
+                  )}
                 </Button>
               );
             })}
           </div>
+
 
           <article className="flex flex-col gap-2 text-justify">
             <AboutMd />
